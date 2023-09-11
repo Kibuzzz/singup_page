102c55b6f61d66be2596066a3b96fb1c124b90bf
kibuzz <kibuzz15@gmail.com>
2023-09-11 17:51:01 +0300

    Working form with db

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
-    <?php
-      $names = ["123", "asdf", "qwpoeraf"];
-      for ($i = 0; $i < count($names); $i++) {
-          echo "{$names[$i]}\n";
-      }
-    ?>
+
+    <h3>Signup</h3>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="email">
+        <button>Submit</button>
+    </form>
+
 </body>
 
 </html>
--- math.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace math;
-function foo() {
-    echo "Hellasdfasjf;adfjo from math";
-}
